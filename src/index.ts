61d152c5850108491af6f028c20a4dffa142a6d2
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-08-18 00:21:33 +0100

    completed eventing system - triggering event callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.on('change', () => { });
-user.on('change', () => { });
-user.on('wijoww', () => { });
+user.on('change', () => { console.log('change #1'); });
+user.on('change', () => { console.log('change #2'); });
+user.on('save', () => { console.log('save #1'); });
 
 console.log(user);
+
+user.trigger('change');
 
