321c5c739c253989ec0fad2f2ec79acf5c992231
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-08-18 14:47:44 +0100

    make Attributes class generic

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,6 +1,6 @@
-export class Attributes {
+export class Attributes<T>{
   //an interface is a type - using an interface to describe an object
-  constructor(private data: UserProps) { }
+  constructor(private data: T) { }
 
   get(propName: string): (number | string) {
     return this.data[propName];
@@ -9,7 +9,7 @@ export class Attributes {
   //argument can be an object that can contain a name as a string or an age as a number
   //set(update: { name: string; age: number }): void {
   //but we'll use an interface instead
-  set(update: UserProps): void {
+  set(update: T): void {
     //overwrite data with all the properties and values of update
     Object.assign(this.data, update);
   }
