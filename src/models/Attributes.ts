700d1a9798d401560b5688b97bd9e5f51fe67efe
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-08-18 14:46:01 +0100

    move get set and contructor from User into Attributes class

---
+++ src/models/Attributes.ts
@@ -0,0 +1,16 @@
+export class Attributes {
+  //an interface is a type - using an interface to describe an object
+  constructor(private data: UserProps) { }
+
+  get(propName: string): (number | string) {
+    return this.data[propName];
+  }
+
+  //argument can be an object that can contain a name as a string or an age as a number
+  //set(update: { name: string; age: number }): void {
+  //but we'll use an interface instead
+  set(update: UserProps): void {
+    //overwrite data with all the properties and values of update
+    Object.assign(this.data, update);
+  }
+}
