3acccc4bcbfb4be75fd1ad7b238c7cbc8caef2ae
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-08-18 13:42:23 +0100

    creating Sync class - a more complicated extraction

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 interface UserProps {
   //make properties optional
@@ -25,24 +24,7 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
 
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      //put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      //post
-      axios.post('http://localhost:3000/users/', this.data);
-    }
-  }
 
 
 }
