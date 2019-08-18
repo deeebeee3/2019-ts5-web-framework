c988fdd980a961fd79f41ecff65e42ff7d9ae624
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-08-18 12:44:56 +0100

    RESTful conventions, json-server uses them, can easily switch to express later

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { User } from './models/User';
+// import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+// const user = new User({ name: 'myname', age: 20 });
 
-user.on('change', () => { console.log('change #1'); });
-user.on('change', () => { console.log('change #2'); });
-user.on('save', () => { console.log('save #1'); });
+// user.on('change', () => { console.log('change #1'); });
+// user.on('change', () => { console.log('change #2'); });
+// user.on('save', () => { console.log('save #1'); });
 
-console.log(user);
+// console.log(user);
 
-user.trigger('change');
+// user.trigger('change');
 
+import axios from 'axios';
+
+//axios.post('http://localhost:3000/users', { name: 'nyName', adge: 20 });
+
+axios.get('http://localhost:3000/users/1');
