ec127802398357e35624c6cb267710ad26e3d135
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-08-18 17:05:52 +0100

    context issue - make sure this is correctly bound to instance of Attributes we create

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,5 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-//we are getting back a reference to the on method on the Eventing class
-const on = user.on;
+console.log(user.get('name'));
 
-on('change', () => {
-  console.log('user was changed');
-});
