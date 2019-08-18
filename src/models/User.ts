19e83d950a16c52c92e10fd68faa7da553a5b6ec
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-08-18 20:28:33 +0100

    Final User Refactor

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
 import { Model } from './Models';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
 export interface UserProps {
   //make properties optional
   id?: number,
@@ -8,5 +11,19 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User extends Model<UserProps>{ }
+export class User extends Model<UserProps>{
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
 }
+
+const user = User.buildUser({});
+user.get('id');
+user.get('name');
+user.get('age');
+user.save();
+user.fetch();
