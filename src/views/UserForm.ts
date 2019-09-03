8eb408d5eeb0d3acce2485e7cd45b92e08f3d9d0
Deepak Bhari <deepakbhari@Deepaks-MacBook-Pro.local>
2019-09-03 11:16:42 +0100

    Binding Event Handlers - adding another event handler

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,7 +6,12 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseover:h1': this.onHeaderHover
     };
+  }
+
+  onHeaderHover(): void {
+    console.log('H1 was hovered over');
   }
 
   onButtonClick(): void {
