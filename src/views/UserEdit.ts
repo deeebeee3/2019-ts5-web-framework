627871a190d7e71aabf1b97386710fa4b02ecdc4
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-09-04 16:38:06 +0100

    Testing Region Mapping

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -2,6 +2,15 @@ import { View } from './View';
 import { User, UserProps } from '../models/User';
 
 export class UserEdit extends View<User, UserProps>{
+
+  //override regionsMap from the View parent Class
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: '.user-show',
+      userForm: '.user-form'
+    };
+  }
+
   template(): string {
     return `
       <div>
